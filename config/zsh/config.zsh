b18a37f658887e84402e03b43b747841dc57197f
Kazuyoshi Akiyama <akiyama.kazuyoshi.64w@gmail.com>
2024-05-19 23:34:16 +0900

    update zsh

--- config/zsh/config.zsh
+++ config/zsh/config.zsh
@@ -1,3 +1,43 @@
+# shell options
+# 同時に起動しているzshの間でhistoryを共有する
+setopt share_history
+# 同じコマンドをhistoryに残さない
+setopt hist_ignore_all_dups
+# historyに保存するときに余分なスペースを削除する
+setopt hist_reduce_blanks
+# 高機能なワイルドカード展開を使用する
+setopt extended_glob
+# cd無しでもディレクトリ移動
+setopt auto_cd
+# コマンドのスペルミスを指摘
+setopt correct
+# historyの重複を記録しない
+setopt hist_ignore_dups
+# historyを編集可能
+setopt hist_verify
+# 補完時にhistoryを自動的に展開
+setopt hist_expand
+# historyをインクリメンタルに保存
+setopt inc_append_history
+# ビープ音無効
+setopt nobeep
+# cd時に自動でpush
+setopt autopushd
+# 同じディレクトリをpushしない
+setopt pushd_ignore_dups
+# TABで保管候補切り替え
+setopt auto_menu
+# --prefix=/usr などの=以降も補完
+setopt magic_equal_subst
+# カッコの対応などを自動で保管
+setopt auto_param_keys
+# ディレクトリ名補完で末尾の/を自動的に補完して次の補完に備える
+setopt auto_param_slash
+# pushd, popdの出力を無効化
+setopt pushd_silent
+# 引数なしのpushdをpushd $HOMEとして扱う
+setopt pushd_to_home
+
 # Ensure path arrays do not contain duplicates.
 typeset -gU cdpath fpath mailpath path
 
